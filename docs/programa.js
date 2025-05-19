1c1bce9b221475ee6632ac27345906c16c57337d
Lorena Triana <trianadiaz2020@gmail.com>
2025-05-19 16:04:43 -0500

    v2

--- docs/programa.js
+++ docs/programa.js
@@ -1,10 +1,10 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([4.639386, -74.082412], 10);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var marker = L.marker([51.5, -0.09]).addTo(map);
+var marker = L.marker([4.639386, -74.082412]).addTo(map);
 
 /*async function cargarPuntos(){
     var miArchivo= await fetch("microondas.geojson")
